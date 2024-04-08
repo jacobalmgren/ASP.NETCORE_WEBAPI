b809dc8255df065f9920a4991d41ee5fdb7e4ee4
Jacob Almgren <almgrenj@proton.me>
2024-04-08 15:45:26 +0200

    changed to checkbox

---
+++ WebAPI/Migrations/20240408125706_AddNewsletterPreferences.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewsletterPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AdvertisingUpdates",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DailyNewsletter",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EventUpdates",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Podcasts",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "StartupWeekly",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WeekInReview",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdvertisingUpdates",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "DailyNewsletter",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "EventUpdates",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "Podcasts",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "StartupWeekly",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "WeekInReview",
+                table: "Subscribers");
+        }
+    }
+}
--- WebAPI/Models/Subscriber.cs
+++ WebAPI/Models/Subscriber.cs
@@ -4,5 +4,11 @@
     {
         public int Id { get; set; }
         public string Email { get; set; }
+        public bool DailyNewsletter { get; set; }
+        public bool AdvertisingUpdates { get; set; }
+        public bool WeekInReview { get; set; }
+        public bool EventUpdates { get; set; }
+        public bool StartupWeekly { get; set; }
+        public bool Podcasts { get; set; }
     }
 }
